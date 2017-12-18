26be9f494ee088c72fc79bb01354cd7b4619f654
tjjunlintx <tianjin.koma@163.com>
2017-12-18 13:36:40 +0800

    网络请求的地址常量

---
+++ app/src/main/java/com/jshc/myapplication/utils/HttpUrlConstant.java
@@ -0,0 +1,11 @@
+package com.jshc.myapplication.utils;
+
+/**
+ * 请求地址常量
+ * Created by JinT on 2017/12/18 0018.
+ */
+
+public class HttpUrlConstant {
+    //菜谱
+    public static final String COOK = "http://apis.juhe.cn/cook/query.php";
+}
