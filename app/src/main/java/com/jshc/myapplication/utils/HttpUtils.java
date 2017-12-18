d214ea3f3f855616f104e3d8d34e70968eab5003
tjjunlintx <tianjin.koma@163.com>
2017-12-18 13:28:22 +0800

    okhttp post

--- app/src/main/java/com/jshc/myapplication/utils/HttpUtils.java
+++ app/src/main/java/com/jshc/myapplication/utils/HttpUtils.java
@@ -8,8 +8,10 @@ import java.util.Map;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 /**
@@ -21,11 +23,11 @@ public class HttpUtils {
     /**
      * GET请求
      *
-     * @param geturl
+     * @param getUrl
      * @param map
      * @param dataService
      */
-    public static void getData(String geturl, Map<String, Object> map, final CallBackDataService dataService) {
+    public static void getData(String getUrl, Map<String, Object> map, final CallBackDataService dataService) {
         OkHttpClient okHttpClient = new OkHttpClient();
         if (!map.isEmpty()) {
             StringBuilder stringBuilder = new StringBuilder();
@@ -35,11 +37,47 @@ public class HttpUtils {
                 stringBuilder.append(entry.getKey() + "=" + entry.getValue() + "&");
             }
             String addUrl = stringBuilder.substring(0, stringBuilder.length() - 1);
-            geturl = geturl + "?" + addUrl;
+            getUrl = getUrl + "?" + addUrl;
         }
         Request request = new Request.Builder()
                 .get()
-                .url(geturl)
+                .url(getUrl)
+                .build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                dataService.Failured(e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                dataService.Successed(response.body().string());
+            }
+        });
+    }
+
+    /**
+     * POST请求
+     *
+     * @param postUrl
+     * @param map
+     * @param dataService
+     */
+    public static void postData(String postUrl, Map<String, Object> map, final CallBackDataService dataService) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        FormBody.Builder builder = new FormBody.Builder();
+        if (!map.isEmpty()) {
+            Iterator<Map.Entry<String, Object>> iterator = map.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry<String, Object> entry = iterator.next();
+                builder.add(entry.getKey(), String.valueOf(entry.getValue()));
+            }
+        }
+        RequestBody requestBody = builder.build();
+        final Request request = new Request.Builder()
+                .post(requestBody)
+                .url(postUrl)
                 .build();
         Call call = okHttpClient.newCall(request);
         call.enqueue(new Callback() {
