51d056b0ff079cf1f2e2f379541f4fab7ad89ab6
tjjunlintx <tianjin.koma@163.com>
2017-12-19 17:13:54 +0800

    image

--- app/src/main/java/com/jshc/waveprogressbar/activitys/SplashActivity.java
+++ app/src/main/java/com/jshc/waveprogressbar/activitys/SplashActivity.java
@@ -1,9 +1,12 @@
 package com.jshc.waveprogressbar.activitys;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.jshc.waveprogressbar.R;
 
@@ -17,6 +20,7 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        decorViewHide();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -25,4 +29,17 @@ public class SplashActivity extends AppCompatActivity {
             }
         }, 3000);
     }
+
+    private void decorViewHide(){
+        //得到当前界面的装饰视图
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            //让应用主题内容占用系统状态栏的空间,注意:下面两个参数必须一起使用 stable 牢固的
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            //设置状态栏颜色为透明
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
 }
