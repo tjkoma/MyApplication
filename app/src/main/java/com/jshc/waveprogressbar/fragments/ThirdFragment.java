98fc42d1ffdac08e05df987e08f357163754ec6e
tjjunlintx <tianjin.koma@163.com>
2017-12-19 17:14:18 +0800

    image

--- app/src/main/java/com/jshc/waveprogressbar/fragments/ThirdFragment.java
+++ app/src/main/java/com/jshc/waveprogressbar/fragments/ThirdFragment.java
@@ -5,21 +5,34 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
 import com.jshc.waveprogressbar.R;
 import com.jshc.waveprogressbar.adapters.ShoppingCartAdapter;
+import com.jshc.waveprogressbar.beans.CookBean;
 import com.jshc.waveprogressbar.beans.GoodBean;
+import com.jshc.waveprogressbar.services.ApiService;
+import com.jshc.waveprogressbar.utils.RetrofitUtils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
 
 /**
  * Created by JinT on 2017/12/15 0015.
@@ -36,6 +49,7 @@ public class ThirdFragment extends Fragment {
 
     private ShoppingCartAdapter shoppingCartAdapter;
     private List<GoodBean> goodList;
+    private ApiService apiService;
 
     @Nullable
     @Override
@@ -57,6 +71,43 @@ public class ThirdFragment extends Fragment {
         shoppingCartAdapter = new ShoppingCartAdapter(getActivity(), goodList);
         thirdRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
         thirdRecyclerView.setAdapter(shoppingCartAdapter);
+//        getCookData();
+    }
+
+    private void getCookData() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("menu", "宫保鸡丁");
+        map.put("key", "88b8e2d806fa3eb422fc0be166a8f452");
+        map.put("dtype", "json");
+        map.put("pn", "1");
+        map.put("rn", "30");
+        map.put("albums", 1);
+        apiService = RetrofitUtils.getInstance().create(ApiService.class);
+        Observable<CookBean> call =  apiService.getCookData(map);
+        call.subscribeOn(Schedulers.newThread())
+                .observeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<CookBean>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(CookBean value) {
+                        Log.e("TAG",new Gson().toJson(value));
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 
     @Override
