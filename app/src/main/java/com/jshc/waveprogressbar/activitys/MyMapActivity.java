3811cef6c589af28cdcd46027e271016cd751b59
tjjunlintx <tianjin.koma@163.com>
2017-12-21 17:24:02 +0800

    amap

--- app/src/main/java/com/jshc/waveprogressbar/activitys/MyMapActivity.java
+++ app/src/main/java/com/jshc/waveprogressbar/activitys/MyMapActivity.java
@@ -1,16 +1,21 @@
 package com.jshc.waveprogressbar.activitys;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.widget.TextView;
 
+import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps.AMap;
-import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.MyLocationStyle;
 import com.jshc.waveprogressbar.R;
 
@@ -32,6 +37,9 @@ public class MyMapActivity extends AppCompatActivity {
     private UiSettings uiSettings;
     private AMapLocationClient aMapLocationClient;
     private AMapLocationClientOption locationClientOption;
+    private boolean followMove = true;
+    private double latitude;
+    private double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +58,7 @@ public class MyMapActivity extends AppCompatActivity {
     private void initView() {
         baseTitleTextView.setText("我的地图");
         uiSettings = aMap.getUiSettings();
-        aMapLocationClient = new AMapLocationClient(this);
+        aMapLocationClient = new AMapLocationClient(getApplicationContext());
         locationClientOption = new AMapLocationClientOption();
         locationClientOption.setOnceLocation(true);//设置单次定位
         //aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
@@ -58,6 +66,17 @@ public class MyMapActivity extends AppCompatActivity {
         aMap.setTrafficEnabled(true);//显示交通状况
         aMap.setMapType(AMap.MAP_TYPE_NAVI);//设置成导航地图
         uiSettings.setMyLocationButtonEnabled(true);
+        /**
+         * 监听位置变化
+         */
+        aMapLocationClient.setLocationListener(new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(AMapLocation aMapLocation) {
+                aMapLocationClient.stopLocation();
+                latitude = aMapLocation.getLatitude();
+                longitude = aMapLocation.getLongitude();
+            }
+        });
     }
 
     //显示定位蓝点
@@ -66,6 +85,23 @@ public class MyMapActivity extends AppCompatActivity {
         myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
         aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
         myLocationStyle.showMyLocation(true);
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE_NO_CENTER);
+        aMap.setOnMyLocationChangeListener(new AMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                if (followMove) {
+                    aMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(latitude, longitude)));
+                }
+            }
+        });
+        aMap.setOnMapTouchListener(new AMap.OnMapTouchListener() {
+            @Override
+            public void onTouch(MotionEvent motionEvent) {
+                if (followMove) {
+                    followMove = false;
+                }
+            }
+        });
     }
 
     @Override
