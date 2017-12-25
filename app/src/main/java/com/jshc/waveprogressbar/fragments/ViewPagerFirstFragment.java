33c2af703381a4ec589418ba3e2edb7972d39476
tjjunlintx <tianjin.koma@163.com>
2017-12-25 17:29:15 +0800

    amap

--- app/src/main/java/com/jshc/waveprogressbar/fragments/ViewPagerFirstFragment.java
+++ app/src/main/java/com/jshc/waveprogressbar/fragments/ViewPagerFirstFragment.java
@@ -44,10 +44,40 @@ public class ViewPagerFirstFragment extends Fragment {
     private void initView() {
         cateList = new ArrayList<>();
         for (int i = 0; i < 8; i++) {
-            CategoryBean bean = new CategoryBean();
-            bean.setImage("http://img2.niutuku.com/1312/0754/0754-niutuku.com-860.jpg");
-            bean.setName("热卖");
-            cateList.add(bean);
+            switch (i) {
+                case 0:
+                    CategoryBean bean0 = new CategoryBean("http://img2.niutuku.com/1312/0754/0754-niutuku.com-860.jpg", "超市");
+                    cateList.add(bean0);
+                    break;
+                case 1:
+                    CategoryBean bean1 = new CategoryBean("http://img2.niutuku.com/desk/1208/1712/ntk-1712-67346.jpg", "全球购");
+                    cateList.add(bean1);
+                    break;
+                case 2:
+                    CategoryBean bean2 = new CategoryBean("http://img2.niutuku.com/desk/1208/1307/ntk-1307-6459.jpg", "服饰");
+                    cateList.add(bean2);
+                    break;
+                case 3:
+                    CategoryBean bean3 = new CategoryBean("http://img2.niutuku.com/desk/1208/1420/ntk-1420-17603.jpg", "生鲜");
+                    cateList.add(bean3);
+                    break;
+                case 4:
+                    CategoryBean bean4 = new CategoryBean("http://img5.imgtn.bdimg.com/it/u=4117439175,1062059118&fm=11&gp=0.jpg", "到家");
+                    cateList.add(bean4);
+                    break;
+                case 5:
+                    CategoryBean bean5 = new CategoryBean("http://tupian.enterdesk.com/2015/mxy/6/14/12/3.jpg", "充值");
+                    cateList.add(bean5);
+                    break;
+                case 6:
+                    CategoryBean bean6 = new CategoryBean("http://img2.niutuku.com/desk/130126/24/24-niutuku.com-1488.jpg", "领券");
+                    cateList.add(bean6);
+                    break;
+                case 7:
+                    CategoryBean bean7 = new CategoryBean("http://img2.niutuku.com/desk/1208/2045/ntk-2045-8214.jpg", "会员");
+                    cateList.add(bean7);
+                    break;
+            }
         }
         categoryAdapter = new CategoryAdapter(getActivity(), cateList);
         viewpagerFirstRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 4));
