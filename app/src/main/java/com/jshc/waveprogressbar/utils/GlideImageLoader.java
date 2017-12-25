98800803cfba825ae42120e3d2037209c203955b
tjjunlintx <tianjin.koma@163.com>
2017-12-25 10:12:24 +0800

    amap

---
+++ app/src/main/java/com/jshc/waveprogressbar/utils/GlideImageLoader.java
@@ -0,0 +1,19 @@
+package com.jshc.waveprogressbar.utils;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.youth.banner.loader.ImageLoader;
+
+/**
+ * banner图像显示类
+ * Created by JinT on 2017/12/25 0025.
+ */
+
+public class GlideImageLoader extends ImageLoader {
+    @Override
+    public void displayImage(Context context, Object path, ImageView imageView) {
+        Glide.with(context).load(path).into(imageView);
+    }
+}
