60a46e0f11d59a70d12312f77940b81d8d5245e3
tjjunlintx <tianjin.koma@163.com>
2017-12-25 11:47:55 +0800

    amap

--- app/src/main/java/com/jshc/waveprogressbar/adapters/ShoppingCartAdapter.java
+++ app/src/main/java/com/jshc/waveprogressbar/adapters/ShoppingCartAdapter.java
@@ -11,6 +11,8 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.jshc.waveprogressbar.R;
 import com.jshc.waveprogressbar.beans.GoodBean;
+import com.jshc.waveprogressbar.utils.GlideImageLoader;
+import com.youth.banner.Banner;
 
 import java.util.List;
 
@@ -21,42 +23,85 @@ import butterknife.ButterKnife;
  * Created by JinT on 2017/12/15 0015.
  */
 
-public class ShoppingCartAdapter extends RecyclerView.Adapter<ShoppingCartAdapter.ViewHolder> {
+public class ShoppingCartAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private Context context;
     private List<GoodBean> list;
+    private List<String> bannerList;
+    private static final int BANNER_ITEM = 0;
+    private static final int RECYCLER_ITEM = 1;
 
-    public ShoppingCartAdapter(Context context, List<GoodBean> list) {
+    public ShoppingCartAdapter(Context context, List<GoodBean> list, List<String> bannerList) {
         this.context = context;
         this.list = list;
+        this.bannerList = bannerList;
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.shopping_cart_item, parent, false);
-        ViewHolder viewHolder = new ViewHolder(view);
+    public int getItemViewType(int position) {
+        switch (position) {
+            case 0:
+                return BANNER_ITEM;
+            default:
+                return RECYCLER_ITEM;
+        }
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder viewHolder = null;
+        switch (viewType) {
+            case BANNER_ITEM:
+                View bannerView = LayoutInflater.from(context).inflate(R.layout.banner_item, parent, false);
+                viewHolder = new BannerViewHolder(bannerView);
+                break;
+            case RECYCLER_ITEM:
+                View view = LayoutInflater.from(context).inflate(R.layout.shopping_cart_item, parent, false);
+                viewHolder = new CommenViewHolder(view);
+                break;
+        }
         return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        Glide.with(context).load("http://img3.redocn.com/20130602/Redocn_2013052815464777.jpg").into(holder.goodImageImageView);
-        holder.goodNameTextView.setText(list.get(position).getGoodName());
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof CommenViewHolder) {
+            Glide.with(context).load("http://img3.redocn.com/20130602/Redocn_2013052815464777.jpg").into(((CommenViewHolder) holder).goodImageImageView);
+            ((CommenViewHolder) holder).goodNameTextView.setText(list.get(realPosition(position)).getGoodName());
+        } else {
+            ((BannerViewHolder) holder).shoppingBanner.setImages(bannerList).setImageLoader(new GlideImageLoader()).start();
+        }
+    }
+
+    public int realPosition(int pos) {
+        return pos - 1;
     }
 
     @Override
     public int getItemCount() {
-        return list.size();
+        if (bannerList.isEmpty()) return list.size();
+        else
+            return list.size() + 1;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class CommenViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.goodImage_imageView)
         ImageView goodImageImageView;
         @BindView(R.id.goodName_textView)
         TextView goodNameTextView;
 
-        public ViewHolder(View itemView) {
-            super(itemView);
-            ButterKnife.bind(this, itemView);
+        public CommenViewHolder(View item) {
+            super(item);
+            ButterKnife.bind(this, item);
+        }
+    }
+
+    public class BannerViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.shopping_Banner)
+        Banner shoppingBanner;
+
+        public BannerViewHolder(View bannerItemView) {
+            super(bannerItemView);
+            ButterKnife.bind(this, bannerItemView);
         }
     }
 }
