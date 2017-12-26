c3a2dc873366be0e7fdbba66d69dc6e7fbc5cc31
tjjunlintx <tianjin.koma@163.com>
2017-12-26 09:45:56 +0800

    amap

---
+++ app/src/main/java/com/jshc/waveprogressbar/activitys/BaseActivity.java
@@ -0,0 +1,19 @@
+package com.jshc.waveprogressbar.activitys;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 基础的Activity
+ * Created by JinT on 2017/12/26 0026.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
