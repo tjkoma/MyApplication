c823e8a5c0395effb086931cf5d918d493911204
tjjunlintx <553788913@qq.com>
2018-03-14 21:08:06 +0800

    presenter

---
+++ app/src/main/java/com/jshc/waveprogressbar/presenters/Fpresenter.java
@@ -0,0 +1,8 @@
+package com.jshc.waveprogressbar.presenters;
+
+/**
+ * Created by tjjunlintx on 2018/3/14.
+ */
+
+public interface Fpresenter {
+}
---
+++ app/src/main/java/com/jshc/waveprogressbar/presenters/FpresenterImpl.java
@@ -0,0 +1,9 @@
+package com.jshc.waveprogressbar.presenters;
+
+/**
+ * Created by tjjunlintx on 2018/3/14.
+ */
+
+public class FpresenterImpl implements Fpresenter {
+    
+}
