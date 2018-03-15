382136a3a0b6ad91641643125de49de6c90d8935
tjjunlintx <tianjin.koma@163.com>
2018-03-15 09:35:55 +0800

    model

---
+++ app/src/main/java/com/jshc/waveprogressbar/modles/Fmodel.java
@@ -0,0 +1,9 @@
+package com.jshc.waveprogressbar.modles;
+
+/**
+ * Created by JinT on 2018/3/15 0015.
+ */
+
+public interface Fmodel {
+    void getData();
+}
---
+++ app/src/main/java/com/jshc/waveprogressbar/modles/FmodelImpl.java
@@ -0,0 +1,12 @@
+package com.jshc.waveprogressbar.modles;
+
+/**
+ * Created by JinT on 2018/3/15 0015.
+ */
+
+public class FmodelImpl implements Fmodel {
+    @Override
+    public void getData() {
+
+    }
+}
